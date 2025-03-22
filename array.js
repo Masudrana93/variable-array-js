a9cfd412b916584afe2dfb7c876c16f40d545eb3
Masud Rana <mr007masudrana@gmail.com>
2025-03-22 11:15:31 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,30 @@
+// Array 
+var employeeSalary = [200, 300, 500, 700, 800];
+
+console.log(employeeSalary);
+
+// length for total data
+var friendsAge = [20, 30, 34, 35, 40];
+
+console.log(friendsAge.length);
+
+var friendsName = ['Anower', "Fysal", "Mamun", 'morshed'];
+
+console.log(friendsName.length);
+
+// indexOf for position
+var projectValue = [50, 100, 120, 150, 180, 200]
+
+console.log(projectValue.indexOf(120));
+
+var projectName = ['Anower', "Fysal", "Mamun",];
+
+console.log(projectName.indexOf('Anower'));
+
+
+
+var projectValue = [50, 100, 120, 150, 180, 200]
+
+var indexElement = projectValue[3];
+
+console.log(indexElement);
---
+++ variable.js
@@ -0,0 +1,32 @@
+// Number 
+var price =typeof 30;
+console.log(price);
+
+
+var price = 30;
+console.log(price);
+
+
+var name = "Masud rana"
+console.log(name);
+
+// string 
+var nameId = "masud";
+console.log(typeof nameId);
+
+
+var price = 30;
+price = 50;
+price = 100;
+console.log(price);
+
+// Boolean
+var y = typeof true;
+console.log(y);
+
+
+var y = true;
+console.log(y);
+
+
+
